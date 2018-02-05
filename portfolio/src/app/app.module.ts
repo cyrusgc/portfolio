b9557abde4bb140a277e808105e516e3106ddd94
jmarrero <cyrusgc@gmail.com>
2018-02-05 16:03:53 +0100

    added routes

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -2,6 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
+// routes
+import {app_routing} from './app.routes';
+
+// components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -20,7 +24,8 @@ import { ItemComponent } from './components/item/item.component';
     ItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    app_routing
   ],
   providers: [],
   bootstrap: [AppComponent]
